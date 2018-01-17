480bd25890d987b26ddae562b0b9ebc3ae05336c
mishfit <mishochu@gmail.com>
2018-01-17 13:27:13 -0700

    stop deploying OCC until deployer takes arguments

--- migrations/3_deploy_original.js
+++ migrations/3_deploy_original.js
@@ -2,11 +2,22 @@ const OriginalToken = artifacts.require("./OriginalToken.sol");
 
 module.exports = function(deployer, network, accounts) {
     const founder = accounts[0],
-          cofounders = accounts.slice(1),
+          airdropCampaign = accounts[1],
+          cofounders = accounts.slice(2),
           decimals = 18,
           tokenName = 'Original Crypto Coin',
           tokenSymbol = 'OCC',
-          OneHundredBillionPlusDecimals = Math.pow(10, (11 + decimals));
+          OneHundredBillionPlusDecimals = Math.pow(10, (11 + decimals)),
+          cofounderDistribution = 55 * Math.pow(10, 8); // 5.5 billion
 
-    deployer.deploy(OriginalToken, cofounders, OneHundredBillionPlusDecimals, tokenName, tokenSymbol);
+// TODO: why doesn't truffle's deployer pass arguments?
+
+/*
+  if (network !== 'development') {
+    deployer.deploy(OriginalToken, cofounders,
+                    airdropCampaign, OneHundredBillionPlusDecimals,
+                    tokenName, tokenSymbol, decimals, cofounderDistribution,  { from: founder });
+  }
+*/
 };
+
