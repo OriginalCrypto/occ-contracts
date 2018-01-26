b98a0eb200bdc165273628321ded9b68acb9b068
mishfit <mishochu@gmail.com>
2018-01-26 10:12:32 -0700

    fallback during tests when loading secrets
    The tests seem to try to execute the migrations.
    Since we hide the accounts of the cofounders
    (not pushed to the repo in plaintext)
    the tests need a fallback to run to completion

--- migrations/3_deploy_original.js
+++ migrations/3_deploy_original.js
@@ -1,27 +1,33 @@
-const OriginalToken = artifacts.require("./OriginalToken.sol"),
-      secret = require('../.cofounders');
+const OriginalToken = artifacts.require("./OriginalToken.sol");
+
+let secret;
+    
 
 module.exports = function(deployer, network, accounts) {
-    const airdropCampaign = secret.airdropCampaignAddress,
-          cofounders = secret.cofounders,
-          decimals = 18,
-          tokenName = 'Original Crypto Coin',
-          tokenSymbol = 'OCC',
-          OneHundredBillionPlusDecimals = Math.pow(10, (11 + decimals)),
-          cofounderDistribution = 55 * Math.pow(10, (8 + decimals)); // 5.5 billion
+  try {
+    secret = require('../.cofounders');
+  } catch (e) {
+    // do not fallback to nonsensical values in production
+    if (e.code !== 'MODULE_NOT_FOUND' ||
+        (network === 'main' || network === 'infura')) {
+      throw e;
+    }
+    
+    secret = { cofounders: accounts.slice(2, 16),
+      airdropCampaignAddress: accounts[1]
+    };
+  }
+
+  const airdropCampaign = secret.airdropCampaignAddress,
+    cofounders = secret.cofounders,
+    decimals = 18,
+    tokenName = 'Original Crypto Coin',
+    tokenSymbol = 'OCC',
+    OneHundredBillionPlusDecimals = Math.pow(10, (11 + decimals)),
+    cofounderDistribution = 55 * Math.pow(10, (8 + decimals)); // 5.5 billion
 
   deployer.deploy(OriginalToken, cofounders,
-                    airdropCampaign, OneHundredBillionPlusDecimals,
-                    tokenName, tokenSymbol, decimals, cofounderDistribution);
-
-// TODO: why doesn't truffle's deployer pass arguments?
-
-/*
-  if (network !== 'development') {
-    deployer.deploy(OriginalToken, cofounders,
-                    airdropCampaign, OneHundredBillionPlusDecimals,
-                    tokenName, tokenSymbol, decimals, cofounderDistribution,  { from: founder });
-  }
-*/
+    airdropCampaign, OneHundredBillionPlusDecimals,
+    tokenName, tokenSymbol, decimals, cofounderDistribution);
 };
 
