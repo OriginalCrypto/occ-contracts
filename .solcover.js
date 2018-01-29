583fdb209a2e78fc4cb495befcceebf5ba4fb21a
mishfit <mishochu@gmail.com>
2018-01-29 03:58:47 -0700

    ignore coverage on stub
    there's a derived contract which allows
    for testing the restricted modifier

--- .solcover.js
+++ .solcover.js
@@ -1,3 +1,7 @@
 module.exports = {
-  skipFiles: [ 'ApprovedAllowanceAgent.sol', 'ERC165.sol', 'ERC20.sol', 'Migrations.sol' ]
+  skipFiles: [  'ApprovedAllowanceAgent.sol',
+                'ERC165.sol',
+                'ERC20.sol',
+                'Migrations.sol',
+                'TestCofoundedRestricted.sol']
 };
