4f65618841fdc2944a42fdc61ad48f62425ea4ee
mishfit <mishochu@gmail.com>
2018-01-29 03:58:03 -0700

    increase test coverage
    check all requires

--- test/TestOwnable.js
+++ test/TestOwnable.js
@@ -4,15 +4,7 @@ const Ownable = artifacts.require('Ownable');
       ownerAfter;
       
 
-async function reverts (p) {
-  try {
-    const result = await p;
-    assert.fail('expected revert but ran to completion.');
-  } catch (e) {
-    const hasReverted = e.message.search(/revert/) > -1;
-    assert(hasReverted, `expected revert but threw ${e}`);
-  }
-}
+
 
 contract('Ownable', function (accounts) {
   const ownerCandidate = accounts[1];
@@ -36,9 +28,29 @@ contract('Ownable', function (accounts) {
   it('prevents non-owners from making ownership changes', async function () {
     assert.equal(ownerBefore, accounts[0]);
 
-    reverts(ownable.setOwner(ownerCandidate, { from: accounts[2] }));
+    await reverts(ownable.setOwner(ownerCandidate, { from: accounts[2] }));
     ownerAfter = await ownable.owner();
     assert.equal(ownerBefore, ownerAfter);
   });
 
+  it('prevents ownership changes to address(0)', async function () {
+
+    assert.equal(ownerBefore, accounts[0]);
+
+    await reverts(ownable.setOwner('0x0000000000000000000000000000000000000000'));
+
+    ownerAfter = await ownable.owner();
+
+    assert.equal(ownerAfter, ownerBefore);
+  });
 });
+
+async function reverts (p) {
+  try {
+    const result = await p;
+    assert.fail('expected revert but ran to completion.');
+  } catch (e) {
+    const hasReverted = e.message.search(/revert/) > -1;
+    assert(hasReverted, `expected revert but threw ${e}`);
+  }
+}
